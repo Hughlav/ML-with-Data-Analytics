a049337e042d63ecdca617df617ae3e569a20f8e
HughLav <HughLavery@Hughs-MacBook-Pro.local>
2017-10-16 20:28:25 +0100

    Implimented a few more regression techniques in sklearn on first dataset.

--- Assignment1/src/Task 1/task1.py
+++ Assignment1/src/Task 1/task1.py
@@ -3,8 +3,17 @@ import sklearn
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+from sklearn import metrics
+import numpy as np
+from sklearn.metrics import r2_score
+from scipy.stats import spearmanr, pearsonr
 
-def linearReg(dataFrame):
+def remodelData(dataFrame):
     #remodle datetime column
     dataFrame['date'], dataFrame['time'] = dataFrame['pickup_datetime'].str.split(' ', 0).str #Split data and time into two features
     del dataFrame['pickup_datetime'] #remover old date+time feature
@@ -14,14 +23,15 @@ def linearReg(dataFrame):
     dataFrame['id'].replace(regex=True, inplace =True, to_replace=r'\D',value=r'')
     #print df
     #print '\n'
+    return dataFrame
 
-
+def linearReg(dataFrame):
     # Split data into X and and Y
     X = dataFrame.drop('trip_duration', axis=1)
     y = dataFrame [['trip_duration']]
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=1)
-
+    
     #Train Model
 
     regression_model = LinearRegression()
@@ -39,6 +49,50 @@ def linearReg(dataFrame):
     print a
     print '\n'
 
+def randomForestRegression(dataFrame):
+    
+    # Split data into X and and Y
+    X = dataFrame.drop('trip_duration', axis=1)
+    y = dataFrame [['trip_duration']]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+    
+    #Standardising data
+    scaler = StandardScaler().fit(X_train)
+    X_train_scaled = pd.DataFrame(scaler.transform(X_train), index=X_train.index.values, columns=X_train.columns.values)
+    X_test_scaled = pd.DataFrame(scaler.transform(X_test), index=X_test.index.values, columns=X_test.columns.values)
+    
+    #Fitting random forsest regressor
+    rfr = RandomForestRegressor(n_estimators=500, oob_score=True, random_state=0)
+    rfr.fit(X_train, y_train.values.ravel())
+    print y_train
+
+    #R squared score
+    #predicted_train = rfr.predict(X_train)
+    #predicted_test = rfr.predict(X_test)
+    #test_score = r2_score(y_test, predicted_test)
+    
+    
+    #print 'Test data R-2 score:{}', test_score
+   
+def logisticReg(dataFrame):
+    # Split data into X and and Y
+    X = dataFrame.drop('trip_duration', axis=1)
+    y = dataFrame [['trip_duration']]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=1)
+    
+    #Train Model
+    lr = LogisticRegression()
+    lr.fit(X_train,y_train)
+    
+    #predictions
+    expect = y_test
+    predict = lr.predict(X_test)
+
+    #summarise fit
+    print (metrics.classification_report(expect,predict))
+    print(metrics.confusion_matrix(expect,predict))
+
 
 
 #NYCdataset = open("/Users/HughLavery/Documents/College/Year 4/CS4404/New York City Taxi Trip Duration/train.csv", "r")
@@ -51,6 +105,14 @@ df10000 = pd.read_csv("/Users/HughLavery/Documents/College/Year 4/CS4404/New Yor
 df50000 = pd.read_csv("/Users/HughLavery/Documents/College/Year 4/CS4404/New York City Taxi Trip Duration/train.csv", nrows=50000)
 df100000 = pd.read_csv("/Users/HughLavery/Documents/College/Year 4/CS4404/New York City Taxi Trip Duration/train.csv", nrows=100000)
 
+df100 = remodelData(df100)
+df500 = remodelData(df500)
+df1000 = remodelData(df1000)
+df5000 = remodelData(df5000)
+df10000 = remodelData(df10000)
+df50000 = remodelData(df50000)
+df100000 = remodelData(df100000)
+
 linearReg(df100)
 linearReg(df500)
 linearReg(df1000)
@@ -59,5 +121,24 @@ linearReg(df10000)
 linearReg(df50000)
 linearReg(df100000)
 
+#randomForestRegression(df100)
+#randomForestRegression(df500)
+#randomForestRegression(df1000)
+#randomForestRegression(df5000)
+#print "5k"
+#randomForestRegression(df10000)
+#print "10k"
+#randomForestRegression(df50000)
+#print "50k"
+#randomForestRegression(df100000)
+#print "100k"
 
+logisticReg(df100)
+logisticReg(df500)
+logisticReg(df1000)
+logisticReg(df5000)
+logisticReg(df10000)
+logisticReg(df50000)
+logisticReg(df100000)
 
+print "done"
